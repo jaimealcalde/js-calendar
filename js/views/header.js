e920a6c619b7915bb6e1ed09f39385aad0630b1b
AntonioCopete <antcopjim@gmail.com>
2021-07-21 17:02:21 +0200

    reset content

--- js/views/header.js
+++ js/views/header.js
@@ -3,21 +3,22 @@ import { header } from "./templates.js";
 import { openModal } from "../modal.js";
 
 function printHeader() {
-	//insert the template HTML in the main.html div calendar
-	let templateHeader = header;
-	wrapper.insertAdjacentHTML("beforeend", templateHeader);
+  wrapper.innerHTML = "";
+  //insert the template HTML in the main.html div calendar
+  let templateHeader = header;
+  wrapper.insertAdjacentHTML("beforeend", templateHeader);
 
-	//clone the template of month
-	let headerNode = document.getElementById("header").content;
-	console.log(headerNode);
-	let copyNode = document.importNode(headerNode, true);
+  //clone the template of month
+  let headerNode = document.getElementById("header").content;
+  console.log(headerNode);
+  let copyNode = document.importNode(headerNode, true);
 
-	//delete de template from the html
-	wrapper.lastChild.remove();
-	wrapper.appendChild(copyNode);
+  //delete de template from the html
+  wrapper.lastChild.remove();
+  wrapper.appendChild(copyNode);
 
-	//button open modal
-	document.getElementById("open-modal").addEventListener("click", openModal);
+  //button open modal
+  document.getElementById("open-modal").addEventListener("click", openModal);
 }
 
 export { printHeader };
